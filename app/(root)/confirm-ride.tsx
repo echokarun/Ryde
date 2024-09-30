ec2ed2e80e0027d651f51aab6d0e69db1b3a83e3
echokarun <rayamajhikarun@gmail.com>
2024-09-30 17:10:11 +0545

    Implement stripe payment system and upload driver info in the db

--- app/(root)/confirm-ride.tsx
+++ app/(root)/confirm-ride.tsx
@@ -8,26 +8,26 @@ import { FlatList, Text, View } from 'react-native'
 
 const ConfirmRide = () => {
 
-   const {drivers, selectedDriver,setSelectedDriver} = useDriverStore()
+   const { drivers, selectedDriver, setSelectedDriver } = useDriverStore()
 
-  return (
-     <RideLayout title='Choose a Driver' snapPoints={["65%", "85%"]}>
-        <FlatList
-           data={drivers}
-           renderItem={({ item }) => (
-              <DriverCard
-                 selected={selectedDriver!}
-                 setSelected={() => setSelectedDriver(Number(item.id)!)}
-                 item={item} />
-        )}
-           ListFooterComponent={() => (
-              <View className='mx-5 mt-10'>
-                 <CustomButton title='Select Ride' onPress={()=> router.push('/(root)/book-ride')} />
-           </View>
-        )}
-        />
-     </RideLayout>
-  )
+   return (
+      <RideLayout title='Choose a Driver' snapPoints={["75%", "85%"]}>
+         <FlatList
+            data={drivers}
+            renderItem={({ item }) => (
+               <DriverCard
+                  selected={selectedDriver!}
+                  setSelected={() => setSelectedDriver(Number(item.id)!)}
+                  item={item} />
+            )}
+            ListFooterComponent={() => (
+               <View className='mx-5 mt-10'>
+                  <CustomButton title='Select Ride' onPress={() => router.push('/(root)/book-ride')} />
+               </View>
+            )}
+         />
+      </RideLayout>
+   )
 }
 
 export default ConfirmRide
